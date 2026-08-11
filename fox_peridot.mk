5da6bf86df66a0f1051025c913ec9b7399a916fe
Azmath Choudhury <azmath2000@gmail.com>
2026-08-11 11:44:57 +0100

    peridot: Block operations after ROM flash
    - Enable operation blocking after a successful ROM install
    - Require users to reboot recovery before further flashing or formatting
    - Reduce slot and data issues caused by continuing in the same recovery session

--- fox_peridot.mk
+++ fox_peridot.mk
@@ -102,3 +102,6 @@ OF_MAINTAINER := azzychy
 # Set to 1 to enable the flashlight feature
 OF_FLASHLIGHT_ENABLE := 1
 OF_FL_PATH1 := /tmp/flashlight
+
+# Set to 1 to block operations after flashing a ROM i.e. formatting data, flashing other zips etc.
+OF_BLOCK_OPERATIONS_AFTER_ROM_FLASH := 1
